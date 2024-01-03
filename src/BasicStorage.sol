47b2e5f9dbe00ffab6d486d94cc90053b524b629
KWAME <antwi1496@hotmail.com>
2024-01-03 14:56:41 -0600

    rectifying my mistake

---
+++ src/BasicStorage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract BasicStorage{
+    // This is a simple program that stores your favorite number as a secret
+    uint256 public data;
+    function storeData(uint256 _data) public {
+        data = _data;
+        bytes32 hashEncodedNumber1 = keccak256(abi.encode(_data));
+        
+        }
+
+    function retrieveData(uint256 _data) public returns(bytes32){
+        data = _data;
+        bytes32 hashEncodedNumber = keccak256(abi.encode(_data));
+        return hashEncodedNumber;
+    }
+}
---
+++ test/BasicStorage.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicStorage} from "../src/BasicStorage.sol";
+import {console} from "forge-std/console.sol";
+
+contract BasicStorageTest is Test{
+    BasicStorage public basicstorage;
+    uint256 number = 4;
+
+    function setUp() public{
+        basicstorage = new BasicStorage();
+    }
+
+    function testStoreData() public {
+        basicstorage.storeData(number);
+        
+    }
+
+    function testRetrieve() public{
+        basicstorage.retrieveData(number);
+    }
+
+}
