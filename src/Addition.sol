cfb9993be5dcf42de360ccadc75de330130b0f5c
KWAME <antwi1496@hotmail.com>
2024-01-06 07:44:36 -0600

    testing contracts

--- src/Addition.sol
+++ src/Addition.sol
@@ -1,18 +1,28 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract BasicAddition{
+contract BasicAddition {
     // This is a simple contract that adds 3 digits
-    uint256 public result;
-    error OverUintLimit();
 
-    function addDigits(uint256 a, uint256 b, uint256 c) public returns (uint256){
+    // result holds the result for the final computtation
+    uint256 public result;
+    
+    // This error indicates the result value is bigger than max of uint256
+    error ArithmeticOverflow();
+
+    /**
+     *
+     * @param a first digit to add
+     * @param b second digit to add
+     * @param c third number to add
+     */
+    function addDigits(uint256 a, uint256 b, uint256 c) public returns (uint256) {
         // this is a check to make sure the variables do not go over their limits
-        if (a> type(uint256).max && b> type(uint256).max && c> type(uint256).max ){
-            revert OverUintLimit();
+        if (a > type(uint256).max || b > type(uint256).max || c > type(uint256).max) {
+            revert ArithmeticOverflow();
         }
+        require((a > type(uint256).min && b > type(uint256).min && c > type(uint256).min), "Underflow");
         result = a + b + c;
         return result;
     }
-
 }
---
+++ test/AdditionTest.t.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {BasicAddition} from "../src/Addition.sol";
+
+contract AdditionTest is Test{
+    BasicAddition add;
+    uint256 firstVal = 1157920892373161954235709850086879078539984665640564039007913129639935;
+    uint256 secondVal = 115792089237316195423570853269984665640564039457584007913129639935;
+    uint256 thirdVal = UINT256_MAX;
+
+    function setUp() public {
+        add = new BasicAddition();
+    }
+
+    function testResultValue() public {
+        // to call the addDigits function
+        add.addDigits(234,456,444);
+        // checking whether results is overflowing or underflowing
+        assertLt(add.result(), type(uint256).max);
+        assertGt(add.result(), type(uint256).min);
+        console.log(add.result());
+    }
+
+    function testAddition() public {
+        // to call the addDigits function
+        add.addDigits(firstVal,secondVal,thirdVal);
+        // checking whether the addition is working properly
+        assertGt(add.result(), secondVal);
+        assertEq(add.result(),firstVal+secondVal+thirdVal);
+    }
+
+    function testRequireMessageInAdd() public {
+        vm.expectRevert(bytes("Underflow"));
+        add.addDigits(firstVal, secondVal, thirdVal);
+
+    }
+
+    function testRevert_error() public {
+        vm.expectRevert();
+        add.addDigits(firstVal,secondVal,thirdVal);
+    }
+
+
+}
