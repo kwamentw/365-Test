07dd859b31a74c9fd7a4f0890564e0bdd1bf3f34
KWAME <antwi1496@hotmail.com>
2024-01-08 05:35:27 -0600

    Testing solidity contracts

--- src/multiplication.sol
+++ src/multiplication.sol
@@ -9,25 +9,18 @@ contract Multiplication {
     error ArithmeticOverUnderflow();
 
     function multiplyUint(uint256 firstNum, uint256 secondNum) external returns (uint256) {
-        if (firstNum > type(uint256).max || secondNum > type(uint256).max) {
-            revert UintOverflow();
-        } else {
             // multiply the two unsigned integers
             result = firstNum * secondNum;
             if (result > type(uint256).max || result < type(uint256).min) {
                 revert ArithmeticOverUnderflow();
             }
             return result;
-        }
     }
 
     function multiplyInt(int256 firstNum, int256 secondNum) external returns (int256) {
-        if (firstNum < type(int256).min || secondNum < type(int256).min) {
-            revert ArithmeticOverUnderflow();
-        } else {
             // multiply the two signed numbers
             results = firstNum * secondNum;
-        }
+        
         if (results < type(int256).min || results > type(int256).max) {
             revert ArithmeticOverUnderflow();
         }
