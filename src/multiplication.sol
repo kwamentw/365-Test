c4653c3b0aa8c384a96294ec9b050162b637e069
KWAME <antwi1496@hotmail.com>
2024-01-08 10:57:08 -0600

    Testing solidity contracts

--- src/multiplication.sol
+++ src/multiplication.sol
@@ -4,12 +4,17 @@ pragma solidity 0.8.20;
 contract Multiplication {
     uint256 public result;
     int256 public  results;
-
-    error UintOverflow();
+    
+    // error to check overflows & underflows
     error ArithmeticOverUnderflow();
 
+    /**
+     * 
+     * @param firstNum First Number of the multiplication
+     * @param secondNum Second Number of the multiplication
+     * multiply the two unsigned integers & returns the resulting value
+     */
     function multiplyUint(uint256 firstNum, uint256 secondNum) external returns (uint256) {
-            // multiply the two unsigned integers
             result = firstNum * secondNum;
             if (result > type(uint256).max || result < type(uint256).min) {
                 revert ArithmeticOverUnderflow();
@@ -17,8 +22,14 @@ contract Multiplication {
             return result;
     }
 
+    /**
+     * 
+     * @param firstNum First Number of the multiplication
+     * @param secondNum Second Number of the multiplication
+     * multiply the two signed numbers & returns the resulting value
+     */
+
     function multiplyInt(int256 firstNum, int256 secondNum) external returns (int256) {
-            // multiply the two signed numbers
             results = firstNum * secondNum;
         
         if (results < type(int256).min || results > type(int256).max) {
