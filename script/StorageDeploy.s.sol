f57eb48d8975a426492352efd36f324aa02aed8d
KWAME <antwi1496@hotmail.com>
2024-01-15 14:30:15 -0600

    Writing, Deploying and Testing Solidity Contracts

--- script/StorageDeploy.s.sol
+++ script/StorageDeploy.s.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.20;
 import {Script} from "forge-std/Script.sol";
 // importing the contract to deploy
 import {BasicStorage} from "../src/BasicStorage.sol";
+import {console} from "forge-std/console.sol";
 
 /**
  * @title A Script that deploys BasicStorage.sol
@@ -12,12 +13,31 @@ import {BasicStorage} from "../src/BasicStorage.sol";
  * @notice i will deploy like a pro
  */
 contract StorageDeploy is Script {
-    function run() external returns (BasicStorage) {
-        // starts to deploy the contract specified
-        vm.startBroadcast();
+    // local anvil chain deploy script
+    // function run() external returns (BasicStorage) {
+    //     // starts to deploy the contract specified
+    //     vm.startBroadcast();
+    //     BasicStorage store = new BasicStorage();
+    //     // ends broadcast of what to deploy
+    //     vm.stopBroadcast();
+    //     return store;
+    // }
+
+    // this function deploys to sepolia testnet
+    function run() public {
+        //access private key
+        uint privateKey = vm.envUint("PRIVATE_KEY");
+        // create account with private key
+        address account = vm.addr(privateKey);
+
+        // print account address
+        console.log("Account", account);
+
+        // deploying
+        vm.startBroadcast(privateKey);
         BasicStorage store = new BasicStorage();
-        // ends broadcast of what to deploy
+        // call this function when deploying
+        store.retrieveData(23);
         vm.stopBroadcast();
-        return store;
     }
 }
---
+++ script/modifierDeploy.s.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {ModifierContract} from "../src/modifiers.sol";
+
+/**
+ * @title A deploy script
+ * @author Kwame 4b
+ * @notice Just trying to deploy on a local chain
+ */
+contract modifierDeploy is Script {
+    // fucntion to deploy contract
+    function run() external returns (ModifierContract) {
+        vm.startBroadcast();
+        ModifierContract modify = new ModifierContract();
+        vm.stopBroadcast();
+        return modify;
+    }
+}
