804f19f88d32fcf3ece9f930a5ba155e65cff1e4
KWAME <antwi1496@hotmail.com>
2024-01-28 15:22:46 -0600

    Writing, Deploying and Testing Solidity Contracts

---
+++ src/AbiEncode.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * @title AbiEncode Contract example
+ * @author Kwame 4b
+ * @notice A simple contract to test out AbiEncode & Decode
+ */
+contract AbiEncodeExample {
+
+}
