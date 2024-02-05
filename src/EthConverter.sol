1d4e0a81daa9098c7339769380d456cea4ec3af2
KWAME <antwi1496@hotmail.com>
2024-02-04 18:05:42 -0600

    Writing, Deploying and Testing Solidity Contracts

--- src/EthConverter.sol
+++ src/EthConverter.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+//import statements
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.4/interfaces/AggregatorV3Interface.sol";
 
+/**
+ * @title A contract to convert eth to usd
+ * @author Kwame 4b
+ * @notice trial contract
+ */
 contract EthConverter {
     address owner;
     uint256 currentPrice;
@@ -13,29 +19,40 @@ contract EthConverter {
         priceFeed = AggregatorV3Interface(_pricefeedaddress);
     }
 
+    // receives usd to be converted
     receive() external payable {}
 
+    /**
+     * get version of current feed in use
+     */
     function getVersion() external view returns (uint256) {
         return priceFeed.version();
     }
 
+    /**
+     * Gets current price of ether in usd
+     */
     function getCurrentPrice() public view returns (uint256) {
         (, int256 currPrice, , , ) = priceFeed.latestRoundData();
         return uint256(currPrice);
     }
 
+    /**
+     * converts eth to usd
+     * @param _amount amount to convert
+     */
     function ConvertETHToUSD(uint256 _amount) public returns (uint256) {
         currentPrice = getCurrentPrice();
         result = _amount * (currentPrice);
         return result;
     }
 
-    function WithDrawUSDEquivalent(
-        address payable receiver,
-        uint256 _amount
-    ) external {
-        result = ConvertETHToUSD(_amount);
-        (bool ok, ) = receiver.call{value: result}("");
-        require(ok, "Transaction was a failure");
-    }
+    // function WithDrawETh(
+    //     address payable receiver,
+    //     uint256 _amount
+    // ) external {
+    //     result = ConvertETHToUSD(_amount);
+    //     (bool ok, ) = receiver.call{value: result}("");
+    //     require(ok, "Transaction was a failure");
+    // }
 }
