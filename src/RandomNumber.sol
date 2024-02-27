26ae9372b6fb47518860d4b1983facf9e1349801
KWAME <antwi1496@hotmail.com>
2024-02-27 17:59:54 -0600

    Writing, Deploying and Testing Contracts

--- src/RandomNumber.sol
+++ src/RandomNumber.sol
@@ -1,4 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract RandomNumber {}
+/**
+ * @title A contract to generate random figures manually
+ * @author Kwame 4B
+ * @notice This contract is for test purposes
+ */
+contract RandomNumber {
+    // event to log generated number
+    event NumberGenerated(uint256);
+
+    //function to generate random number
+    function generateNumber() public returns (uint256) {
+        uint256 RandNum = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
+            )
+        );
+        emit NumberGenerated(RandNum);
+        return RandNum;
+    }
+}
