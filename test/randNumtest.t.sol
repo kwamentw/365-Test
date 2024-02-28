a27dd194ab008dce379cc92c7c13d59267b03bed
KWAME <antwi1496@hotmail.com>
2024-02-28 15:09:06 -0600

    Writing, Deploying and Testing Contracts

--- test/randNumtest.t.sol
+++ test/randNumtest.t.sol
@@ -1,7 +1,23 @@
 //SPDX-License-identifier:MIT
 pragma solidity 0.8.20;
 
+//import statements
 import {RandomNumber} from "../src/RandomNumber.sol";
 import {Test} from "forge-std/Test.sol";
 
-contract TestRandomNumber is Test {}
+contract TestRandomNumber is Test {
+    //initializing contract
+    RandomNumber rand;
+
+    function setUp() public {
+        //setting up contract
+        rand = new RandomNumber();
+    }
+
+    /*
+     *Testing if number is being generated
+     */
+    function testGenerateNum() public {
+        rand.generateNumber();
+    }
+}
