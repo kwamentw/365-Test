c24a183bec1ee831bee3ffec7ab0b2856261d1ea
KWAME <antwi1496@hotmail.com>
2024-02-28 15:00:14 -0600

    Writing, Deploying and Testing Contracts

---
+++ test/randNumtest.t.sol
@@ -0,0 +1,7 @@
+//SPDX-License-identifier:MIT
+pragma solidity 0.8.20;
+
+import {RandomNumber} from "../src/RandomNumber.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract TestRandomNumber is Test {}
