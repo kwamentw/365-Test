b1b26c5afb72669d004c86c3f1e3c2aa6acb02e0
KWAME <antwi1496@hotmail.com>
2024-03-01 17:32:24 -0600

    Writing, Deploying and Testing Contracts

---
+++ script/RandNumDeploy.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {RandomNumber} from "../src/RandomNumber.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract RandNumDeploy {}
