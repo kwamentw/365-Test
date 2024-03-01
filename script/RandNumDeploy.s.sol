bfa6e51315ff8241a9eec7fcd8dcd796a72b779c
KWAME <antwi1496@hotmail.com>
2024-03-01 17:35:44 -0600

    Writing, Deploying and Testing Contracts

--- script/RandNumDeploy.s.sol
+++ script/RandNumDeploy.s.sol
@@ -4,4 +4,14 @@ pragma solidity 0.8.20;
 import {RandomNumber} from "../src/RandomNumber.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract RandNumDeploy {}
+contract RandNumDeploy is Script {
+    RandomNumber rand;
+
+    function run() public returns (RandomNumber) {
+        vm.startBroadcast();
+        rand = new RandomNumber();
+        vm.stopBroadcast();
+
+        return rand;
+    }
+}
