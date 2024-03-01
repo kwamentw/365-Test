6a66e0a00def5026c4b654fb99e99b25022c19ad
KWAME <antwi1496@hotmail.com>
2024-03-01 17:39:36 -0600

    Writing, Deploying and Testing Contracts

--- script/RandNumDeploy.s.sol
+++ script/RandNumDeploy.s.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+//import scripts
 import {RandomNumber} from "../src/RandomNumber.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract RandNumDeploy is Script {
+    //initializing contract
     RandomNumber rand;
 
+    /**
+     * Deploying contract to local chain
+     */
     function run() public returns (RandomNumber) {
         vm.startBroadcast();
         rand = new RandomNumber();
