d4e54c9e70a8a812e0510c84c88ed36039a03ca3
KWAME <antwi1496@hotmail.com>
2024-03-02 18:22:02 -0600

    Writing, Deploying and Testing Contracts

---
+++ src/ChainlinkRandNumber.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+/**
+ * @title Random number generator
+ * @author Kwame 4B
+ * @notice A contract to generate a random number using chainlink vrf
+ */
+contract OracleRandomNumber {
+
+}
