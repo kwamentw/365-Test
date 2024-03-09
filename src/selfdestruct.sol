94a63d92067d9a6f3816f71bd8a787faff03c4ad
KWAME <antwi1496@hotmail.com>
2024-03-09 14:37:33 -0600

    Writing, Deploying and Testing Contracts

--- src/selfdestruct.sol
+++ src/selfdestruct.sol
@@ -2,32 +2,50 @@
 pragma solidity 0.8.20;
 
 contract FamilySubscription {
+    //////////// event ////////////////
     event IsSubscriptionRenewed(bool);
+
+    ///////// errors ///////////
     error Insufficient_Funds();
     error Invalid_Value();
 
+    ///////// state variables ////////////
     uint256 public targetAmount = 5 ether;
     bool SubscriptionRenewed;
     address SubscriptionManager;
 
     /**
+     * to accept members contribution
      * its a family of 5
      * Every has to send equal contribution of 1 ether
      */
     function DepositSubscription() public payable {
         if (msg.value == 1 ether) {
             uint256 balance = address(this).balance;
-            if (balance < 5 ether) {
+            if (balance < targetAmount) {
                 revert Insufficient_Funds();
             }
-            (bool sent, ) = SubscriptionManager.call{value: 5 ether}("");
-            require(sent, "Txn failed");
-            SubscriptionRenewed = true;
         } else {
             revert Invalid_Value();
         }
     }
 
+    /**
+     * To renew the subscription
+     */
+    function RenewSubscription() public returns (bool) {
+        if (address(this).balance != 5 ether) {
+            revert Insufficient_Funds();
+        }
+        (bool sent, ) = SubscriptionManager.call{value: 5 ether}("");
+        require(sent, "Txn failed");
+        SubscriptionRenewed = true;
+        return SubscriptionRenewed;
+    }
+
+    /**
+     * To check the status of the susbscription
+     */
     function checkSubscription() public {
         emit IsSubscriptionRenewed(SubscriptionRenewed);
     }
