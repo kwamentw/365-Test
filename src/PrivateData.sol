29587f7101de635cc2257c4d8888a150101bcd62
KWAME <antwi1496@hotmail.com>
2024-03-10 18:35:05 -0500

    Writing, Deploying and Testing Contracts

---
+++ src/PrivateData.sol
@@ -0,0 +1,11 @@
+// SPDx-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * @title Accessing Private Data
+ * @author KWame4b
+ * @notice A contract to read private data
+ */
+contract PrivateData {
+
+}
