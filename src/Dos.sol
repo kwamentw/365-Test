a2e3ad9c1f9d35844819b112ea5a7ad54407317d
KWAME <antwi1496@hotmail.com>
2024-03-12 17:32:42 -0500

    Writing, Deploying and Testing Contracts

--- src/Dos.sol
+++ src/Dos.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Dos {}
+contract Dos {
+    address public king;
+    uint256 public balance;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "You need to pay more to become the king");
+        (bool sent, ) = king.call{value: balance}("");
+        require(sent, "Failed to send ether");
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+}
