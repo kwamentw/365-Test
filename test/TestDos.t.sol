869ec21ee212a687b5e1d5742af38934517bc5c6
KWAME <antwi1496@hotmail.com>
2024-03-12 19:22:03 -0500

    Writing, Deploying and Testing Contracts

--- test/TestDos.t.sol
+++ test/TestDos.t.sol
@@ -1,8 +1,5 @@
 //SPDX-License-Identifier:MIT
 pragma solidity 0.8.20;
-// seems nothing wants to commit
-// dont even know what to do
-// like fr wtf?
 
 import {Test} from "forge-std/Test.sol";
 import {Dos} from "../src/Dos.sol";
@@ -25,3 +22,5 @@ contract DosTest is Test {
         assert(false);
     }
 }
+
+// 13th march commit didnt show on the profile overview
