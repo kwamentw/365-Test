1fa55b56510a7a405006e765ef0b3b98402f71ab
KWAME <antwi1496@hotmail.com>
2024-03-13 18:53:53 -0500

    Writing, Deploying and Testing Contracts

--- src/Frontrunning.sol
+++ src/Frontrunning.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Frontrun {}
+/**
+ * @title A frontrun vulnerable contract
+ * @author Solidity by example
+ * @notice trying to understand the concept more
+ */
+contract Frontrun {
+
+}
